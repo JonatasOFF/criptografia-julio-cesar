04333381445e3ef9b43a832ca83057b38794c180
Jonatas Oliveira Ferreira <38872572+JonatasOFF@users.noreply.github.com>
2020-02-20 12:13:23 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+const axios = require("axios");
+const sha1 = require("js-sha1");
+
+const TOKEN = "a5b9923cd9749e95e27bcc276c1597bf9e8dd969";
+
+fs = require("fs");
+
+const analfabeto = "abcdefghijklmnopqrstuvwxyz".split("");
+
+axios({
+        method: "get",
+        url: "https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=" +
+            TOKEN
+    })
+    .then(result => {
+        let data = result.data;
+
+        let resultado = descriptografar(
+            result.data.cifrado,
+            result.data.numero_casas
+        );
+
+        data.decifrado = resultado;
+        data.resumo_criptografico = sha1(resultado);
+
+        fs.writeFileSync("answer.json", JSON.stringify(data));
+    })
+    .catch(err => {
+        console.log(err);
+    });
+
+// function criptografar(frase, casas) {
+//     let palavras = frase.toLowerCase();
+//     let res = new RegExp("[a-z]");
+//     let criptografado = "";
+
+//     palavras.split("").forEach(k => {
+//         if (res.test(k)) {
+//             count = analfabeto.indexOf(k) + casas;
+//             criptografado += analfabeto[count > 26 ? count - 26 : count];
+//         } else {
+//             criptografado += k;
+//         }
+//     });
+//     return criptografado;
+// }
+
+function descriptografar(frase, casas) {
+    let palavras = frase.toLowerCase();
+    let res = new RegExp("[a-z]");
+    let descriptografado = "";
+
+    palavras.split("").forEach(k => {
+        if (res.test(k)) {
+            count = analfabeto.indexOf(k) - casas;
+            descriptografado += analfabeto[count < 0 ? count + 26 : count];
+        } else {
+            descriptografado += k;
+        }
+    });
+    return descriptografado;
+}
